3a09ed11ce84fbeff166f78bec4ae0d990d4e122
Alex Loayza <alexht129@gmail.com>
2023-02-04 14:35:46 -0500

    Deploy base project

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import './bootstrap.bundle.min.js';
+
+/*
+ * A partir de aqui puedes escribir codigo JS personalizado
+ * Si deseas importar otros modulos que sean debajo de la linea 1
+ * @example => import 'newmodule.js';
+ */
+
+// alert('Go!')
